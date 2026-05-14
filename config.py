64aad4b3eb5a6c815aa5ddef4c4ce232126fab7c
olivers2028 <oliverkaistapleton@gmail.com>
2026-05-14 11:00:18 +0800

    Blank config

---
+++ config.py
@@ -0,0 +1,22 @@
+config = {
+    "model":"",
+
+    "host":"",
+    "username":"",
+    "key_path":"/",
+    "passphrase":None,
+
+    "ocr_model":"deepseek_ocr",
+
+    "remote_host":"127.0.0.1",
+    "remote_port":11434,
+
+    "ollama_url":"http://127.0.0.1:11500",
+
+    "local_host":"127.0.0.1",
+    "local_port":11500,
+
+    "pdf_path":"data/mydocs.pdf",
+
+}
+
